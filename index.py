1760ab42d799106ac0eb3e7c70b08d1f347d7197
lmoroco <lmorocoramos@gmail.com>
2023-12-31 13:30:44 -0500

    feat: unnecessary file removed

--- index.py
+++
@@ -1,7 +0,0 @@
-mi_diccionario = {"a": 1, "b": 2, "c": 3}
-
-# Actualizar valores en una sola línea
-mi_diccionario = {clave: valor * 2 for clave, valor in mi_diccionario.items()}
-
-# Imprimir el diccionario actualizado
-print(mi_diccionario)
